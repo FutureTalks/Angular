def7541da9b7231a91d7c09e7e0cd797281068e5
broggi <sebi.broggi@gmail.com>
2019-05-06 23:22:48 +0200

    Flappybirds only on PC

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -1,8 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { DogService } from 'src/app//services/content/dog.service';
 import { Content } from 'src/app/models/Content';
 import { Accordion } from 'src/app/models/Accordion';
 import { ContentService } from 'src/app/services/content/content.service';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-projects',
@@ -14,18 +17,19 @@ export class ProjectsComponent implements OnInit {
   content: Content;
   source="";
   talks: Accordion[];
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+  .pipe(
+    map(result => result.matches)
+  );
   
-  constructor(private dogService: DogService, private firebase: ContentService){
+  constructor(private breakpointObserver: BreakpointObserver, private dogService: DogService, private firebase: ContentService){
     this.firebase.getAccordion('talks').subscribe(items => this.talks = items.sort((a, b) => b.order - a.order));
     this.firebase.getContent('Projects').subscribe(items => this.content = items[0]);
-    this.getDog();
-  }
-
-  ngOnInit(){
-  }
-
-  getDog(){
     this.dogService.get().subscribe(resp => this.source=resp.message);
+  }
+
+  ngOnInit(){ 
   }
 
 }
